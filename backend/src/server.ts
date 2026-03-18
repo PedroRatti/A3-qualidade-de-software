3bc6feab618dc4381396f8ef3f6375954b8d450a
Pedro Ratti <pedrohratti@gmail.com>
2026-03-18 15:49:06 -0300

    ajustando arquivo playwright.config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log("Servidor rodando em http://localhost:${PORT}");
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
